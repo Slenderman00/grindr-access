d6a0d6f9ad9fec089296f21e9e666a39292d0af2
Joar Heimonen <joarheimonen@live.no>
2023-11-01 00:38:23 +0100

    added loop

--- xmpp.py
+++ xmpp.py
@@ -10,5 +10,7 @@ def connect(plainToken):
         with context.wrap_socket(sock, server_hostname=hostname) as secure_sock:
             print(secure_sock.version())
             secure_sock.send(f"<session to='chat.grindr.com' auth_data='{plainToken}' resource='3e6f228230b9c7b3' stream_management='true' carbons='true' compress='false'>".encode())
-            msgReceived = secure_sock.recv(2048)
-            print(msgReceived.decode())
+            while True:
+                msgReceived = secure_sock.recv(2048)
+                if msgReceived:
+                    print(msgReceived.decode())
