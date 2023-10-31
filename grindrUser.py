b0f75979a85f88258cd8e66b8aabe614ca3e23d1
Joar Heimonen <joarheimonen@live.no>
2023-10-31 23:43:40 +0100

    more shit

--- grindrUser.py
+++ grindrUser.py
@@ -34,26 +34,46 @@ class grindrUser:
 
         response = genericGet(GET_USERS, params, auth_token=self.sessionId)
         return response
-    
+
     def getTaps(self):
         response = genericGet(TAPS_RECIEVED, {}, auth_token=self.sessionId)
         return response
 
     # type is a number from 1 - ?
     def tap(self, profileId, type):
-        response = genericPost(TAP, {"recipientId": profileId, "tapType": type}, auth_token=self.sessionId)
+        response = genericPost(
+            TAP, {"recipientId": profileId, "tapType": type}, auth_token=self.sessionId
+        )
         return response
 
     def getProfile(self, profileId):
         response = genericGet(GET_PROFILE + profileId, {}, auth_token=self.sessionId)
         return response
-    
+
     # profileIdList MUST be an array of profile ids
     def getProfileStatuses(self, profileIdList):
-        response = genericPost(STATUS, {"profileIdList": profileIdList}, auth_token=self.sessionId)
+        response = genericPost(
+            STATUS, {"profileIdList": profileIdList}, auth_token=self.sessionId
+        )
         return response
-    
+
     def getAlbum(self, profileId):
-        response = genericPost(ALBUM, {"profileId": profileId}, auth_token=self.sessionId)
+        response = genericPost(
+            ALBUM, {"profileId": profileId}, auth_token=self.sessionId
+        )
         return response
-    
+
+    #returns session data (might renew it)
+    def sessions(self, email):
+        response = genericPost(
+            SESSIONS,
+            {"email": email, "token": "", "authToken": self.authToken},
+            auth_token=self.sessionId,
+        )
+
+        self.sessionId = response["sessionId"]
+        self.profileId = response["profileId"]
+        self.authToken = response["authToken"]
+        self.xmppToken = response["xmppToken"]
+
+        return response
