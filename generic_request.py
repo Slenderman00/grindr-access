edb10abe1fe36e97e7f37ab559f6c6c1022357d0
Joar Heimonen <contact@joar.me>
2024-10-30 12:59:30 +0100

    feat: updated the framework

--- generic_request.py
+++ generic_request.py
@@ -1,16 +1,69 @@
 import pycurl
 import json
 import zlib
-from utils import gen_l_dev_info
+from .utils import gen_l_dev_info
 
 
-def generic_post(path, data, auth_token=None):
+def generic_jpeg_upload(path, image_io, auth_token=None, proxy=None, proxy_port=None):
+    buffer = image_io.read()
+
+    response_data = []
+
+    c = pycurl.Curl()
+
+    if proxy is not None and proxy_port is not None:
+        c.setopt(c.PROXY, proxy)
+        c.setopt(c.PROXYPORT, proxy_port)
+
+    c.setopt(c.URL, "https://grindr.mobi" + path)
+    c.setopt(c.CUSTOMREQUEST, "POST")
+
+    headers = [
+        "accept: application/json",
+        "accept-encoding: gzip",
+        "accept-language: en-US",
+        "connection: Keep-Alive",
+        "content-type: image/jpeg",
+        "host: grindr.mobi",
+        f"l-device-info: {gen_l_dev_info()}",
+        "l-locale: en_US",
+        "l-time-zone: Europe/Oslo",
+        "requirerealdeviceinfo: true",
+        "user-agent: grindr3/24.12.0.125239;125239;Free;Android 14;sdk_gphone64_x86_64;Google",
+    ]
+
+    if auth_token is not None:
+        headers.append("authorization: Grindr3 " + auth_token)
+
+    c.setopt(c.HTTPHEADER, headers)
+
+    c.setopt(c.POSTFIELDS, buffer)
+
+    def handle_response(data):
+        response_data.append(data)
+
+    c.setopt(c.WRITEFUNCTION, handle_response)
+    c.perform()
+    c.close()
+
+    response_data = b"".join(response_data)
+    decompressed_response = zlib.decompress(response_data, zlib.MAX_WBITS | 16)
+    return json.loads(decompressed_response)
+
+
+def generic_post(path, data, auth_token=None, proxy=None, proxy_port=None):
     response_data = []
 
     request_data = data
 
     data_json = json.dumps(request_data)
     c = pycurl.Curl()
+
+    if proxy is not None and proxy_port is not None:
+        c.setopt(c.PROXY, proxy)
+        c.setopt(c.PROXYPORT, proxy_port)
+        # c.setopt(c.PROXYTYPE, pycurl.PROXYTYPE_HTTP)
+
     c.setopt(c.URL, "https://grindr.mobi" + path)
     c.setopt(c.CUSTOMREQUEST, "POST")
 
@@ -25,7 +78,7 @@ def generic_post(path, data, auth_token=None):
         "l-locale: en_US",
         "l-time-zone: Europe/Oslo",
         "requirerealdeviceinfo: true",
-        "user-agent: grindr3/24.7.0.118538;118538;Free;Android 14;sdk_gphone64_x86_64;Google",
+        "user-agent: grindr3/24.12.0.125239;125239;Free;Android 14;sdk_gphone64_x86_64;Google",
     ]
 
     if auth_token is not None:
@@ -47,12 +100,74 @@ def generic_post(path, data, auth_token=None):
     return json.loads(decompressed_response)
 
 
-def generic_get(path, data, auth_token=None):
+def generic_put(path, data, auth_token=None, proxy=None, proxy_port=None):
+    response_data = []
+
+    request_data = data
+
+    data_json = json.dumps(request_data)
+    c = pycurl.Curl()
+
+    if proxy is not None and proxy_port is not None:
+        c.setopt(c.PROXY, proxy)
+        c.setopt(c.PROXYPORT, proxy_port)
+        # c.setopt(c.PROXYTYPE, pycurl.PROXYTYPE_HTTP)
+
+    c.setopt(c.URL, "https://grindr.mobi" + path)
+    c.setopt(c.CUSTOMREQUEST, "PUT")
+
+    headers = [
+        "accept: application/json",
+        "accept-encoding: gzip",
+        "accept-language: en-US",
+        "connection: Keep-Alive",
+        "content-type: application/json; charset=UTF-8",
+        "host: grindr.mobi",
+        f"l-device-info: {gen_l_dev_info()}",
+        "l-locale: en_US",
+        "l-time-zone: Europe/Oslo",
+        "requirerealdeviceinfo: true",
+        "user-agent: grindr3/24.12.0.125239;125239;Free;Android 14;sdk_gphone64_x86_64;Google",
+        "Content-Length: " + str(len(data_json))
+    ]
+
+    if auth_token is not None:
+        headers.append("authorization: Grindr3 " + auth_token)
+
+    c.setopt(c.HTTPHEADER, headers)
+
+    c.setopt(c.POSTFIELDS, data_json)
+
+    def handle_response(data):
+        response_data.append(data)
+
+    c.setopt(c.WRITEFUNCTION, handle_response)
+    c.perform()
+    c.close()
+
+    response_data = b"".join(response_data)
+    try:
+        decompressed_response = zlib.decompress(response_data, zlib.MAX_WBITS | 16)
+    except zlib.error as e:
+        return {"error": "Decompression failed", "details": str(e)}
+
+    try:
+        return json.loads(decompressed_response)
+    except json.JSONDecodeError as e:
+        return {"error": "JSON decoding failed", "details": str(e)}
+
+
+def generic_get(path, data, auth_token=None, proxy=None, proxy_port=None):
     response_data = []
 
     request_data = data
 
     c = pycurl.Curl()
+
+    if proxy is not None and proxy_port is not None:
+        c.setopt(c.PROXY, proxy)
+        c.setopt(c.PROXYPORT, proxy_port)
+        # c.setopt(c.PROXYTYPE, pycurl.PROXYTYPE_HTTP)
 
     c.setopt(
         c.URL,
--- grindr_user.py
+++ grindr_user.py
@@ -1,6 +1,16 @@
-from generic_request import generic_post, generic_get
-from paths import SESSIONS, TAP, GET_USERS, TAPS_RECIEVED, GET_PROFILE, STATUS, ALBUM
-from utils import to_geohash
+from .generic_request import generic_post, generic_get, generic_put, generic_jpeg_upload
+from .paths import (
+    SESSIONS,
+    TAP,
+    GET_USERS,
+    TAPS_RECIEVED,
+    GET_PROFILE,
+    STATUS,
+    ALBUM,
+    PROFILE,
+    IMAGES
+)
+from .utils import to_geohash
 import binascii
 
 
@@ -11,16 +21,26 @@ class GrindrUser:
         self.authToken = None
         self.xmppToken = ""
 
+        self.proxy = None
+        self.proxy_port = None
+
+    def set_proxy(self, proxy, proxy_port):
+        self.proxy = proxy
+        self.proxy_port = proxy_port
+
     def login(self, email, password):
         response = generic_post(
-            SESSIONS, {"email": email, "password": password, "token": ""}
+            SESSIONS,
+            {"email": email, "password": password, "token": ""},
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
         )
-        print(response)
         if "code" in response:
             code = response["code"]
 
             if code == 30:
-                print("You need to verify your account via phone number!")
+                raise Exception("You need to verify your account via phone number!")
+
         self.sessionId = response["sessionId"]
         self.profileId = response["profileId"]
         self.authToken = response["authToken"]
@@ -38,34 +58,64 @@ class GrindrUser:
             "rightNow": "false",
         }
 
-        response = generic_get(GET_USERS, params, auth_token=self.sessionId)
+        response = generic_get(
+            GET_USERS,
+            params,
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
+        )
         return response
 
     def get_taps(self):
-        response = generic_get(TAPS_RECIEVED, {}, auth_token=self.sessionId)
+        response = generic_get(
+            TAPS_RECIEVED,
+            {},
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
+        )
         return response
 
     # type is a number from 1 - ?
     def tap(self, profileId, type):
         response = generic_post(
-            TAP, {"recipientId": profileId, "tapType": type}, auth_token=self.sessionId
+            TAP,
+            {"recipientId": profileId, "tapType": type},
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
         )
         return response
 
     def get_profile(self, profileId):
-        response = generic_get(GET_PROFILE + profileId, {}, auth_token=self.sessionId)
+        response = generic_get(
+            GET_PROFILE + profileId,
+            {},
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
+        )
         return response
 
     # profileIdList MUST be an array of profile ids
     def get_profile_statuses(self, profileIdList):
         response = generic_post(
-            STATUS, {"profileIdList": profileIdList}, auth_token=self.sessionId
+            STATUS,
+            {"profileIdList": profileIdList},
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
         )
         return response
 
     def get_album(self, profileId):
         response = generic_post(
-            ALBUM, {"profileId": profileId}, auth_token=self.sessionId
+            ALBUM,
+            {"profileId": profileId},
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
         )
         return response
 
@@ -75,6 +125,8 @@ class GrindrUser:
             SESSIONS,
             {"email": email, "token": "", "authToken": self.authToken},
             auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
         )
 
         self.sessionId = response["sessionId"]
@@ -82,6 +134,16 @@ class GrindrUser:
         self.authToken = response["authToken"]
         self.xmppToken = response["xmppToken"]
 
+        return response
+
+    def update_profile(self, data):
+        response = generic_put(
+            PROFILE,
+            data,
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
+        )
         return response
 
     # generating plain auth
@@ -98,3 +160,20 @@ class GrindrUser:
         _hex = str(_hex)
         _hex = _hex.replace("b'", "").replace("'", "")
         return _hex
+
+    def upload_image(self, file_io):
+        return generic_jpeg_upload(
+            "/v3/me/profile/images?thumbCoords=194%2C0%2C174%2C20",
+            file_io,
+            auth_token=self.sessionId,
+            proxy=self.proxy,
+            proxy_port=self.proxy_port,
+        )
+
+    def set_image(self, primary_hash, secondary_hashes=[]):
+        data = {
+            "primaryImageHash": primary_hash,
+            "secondaryImageHashes": secondary_hashes,
+        }
+
+        return generic_put(IMAGES, data, auth_token=self.sessionId, proxy=self.proxy, proxy_port=self.proxy_port)
